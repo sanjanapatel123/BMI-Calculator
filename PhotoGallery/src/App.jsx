c5bb1a4a7a8909ae19067b985edf2a0ddf1f5a44
sanjana patel <sanjanapatel438@gmail.com>
2025-01-11 00:14:19 +0530

    first commit

---
+++ PhotoGallery/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Gallery from "./components/Gallery";
+
+const App = () => {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Image Gallery</h1>
+      <Gallery />
+    </>
+  );
+};
+
+export default App;
